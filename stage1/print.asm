7d1991f8e6475f774263bdc5a0be15c9c0b79068
Lizzy Fleckenstein <eliasfleckenstein@web.de>
2023-12-07 14:52:20 +0100

    include 16-bit number printing functions in stage1

--- stage1/print.asm
+++ stage1/print.asm
@@ -10,3 +10,50 @@ print_str:
 	jmp .print
 .return:
 	ret
+
+; modify eax, ebx, ecx, edx
+print_hex:
+	mov ebx, 0x10
+	jmp print_num
+print_dec:
+	mov ebx, 10
+print_num:
+	xor ecx, ecx
+.convert:
+	inc ecx
+	xor edx, edx
+	div ebx
+	cmp dl, 10
+	jb .digit
+	add dl, 'A'-10
+	jmp .next
+.digit:
+	add dl, '0'
+.next:
+	push dx
+	cmp eax, 0
+	jne .convert
+.print:
+	cmp ecx, 0
+	je .return
+	dec ecx
+	pop ax
+	mov ah, 0x0E
+	int 0x10
+	jmp .print
+.return:
+	ret
+
+newline:
+	mov al, 10
+	call print_chr
+
+	mov al, 13
+	call print_chr
+
+	ret
+
+print_chr:
+	mov ah, 0x0E
+	int 0x10
+	ret
--- stage2/main.asm
+++ stage2/main.asm
@@ -33,53 +33,6 @@ setup:
 %include "stage2/paging.asm"
 %include "stage1/print.asm"
 
-; modify eax, ebx, ecx, edx
-print_hex:
-	mov ebx, 0x10
-	jmp print_num
-print_dec:
-	mov ebx, 10
-print_num:
-	xor ecx, ecx
-.convert:
-	inc ecx
-	xor edx, edx
-	div ebx
-	cmp dl, 10
-	jb .digit
-	add dl, 'A'-10
-	jmp .next
-.digit:
-	add dl, '0'
-.next:
-	push dx
-	cmp eax, 0
-	jne .convert
-.print:
-	cmp ecx, 0
-	je .return
-	dec ecx
-	pop ax
-	mov ah, 0x0E
-	int 0x10
-	jmp .print
-.return:
-	ret
-
-newline:
-	mov al, 10
-	call print_chr
-
-	mov al, 13
-	call print_chr
-
-	ret
-
-print_chr:
-	mov ah, 0x0E
-	int 0x10
-	ret
-
 [bits 64]
 
 long_mode:
