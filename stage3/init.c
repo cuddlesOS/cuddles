43a856b5d40ceb570af7755d9f1222093a48aa64
Kimapr <kimapr@mail.ru>
2023-12-21 13:09:53 +0500

    move SSE init code to init, add -mgeneral-regs-only to init.c and pic.c

--- stage3/init.c
+++ stage3/init.c
@@ -1,6 +1,18 @@
+#include "def.h"
 void kmain();
 
 void init()
 {
+	// enable SSE. long mode demands it is present
+	u64 cr0;
+	asm volatile("mov %%cr0, %0 \n":"=r"(cr0));
+	asm volatile("mov %0, %%cr0"::"r"((cr0 & ~(1 << 2)) | (1 << 1)));
+
+	u64 cr4;
+	asm volatile("mov %%cr4, %0":"=r"(cr4));
+	asm volatile("mov %0, %%cr4"::"r"(cr4 | (1 << 9) | (1 << 10)));
+
+	u16 fpu_cw = 0x37a;
+	asm volatile("fldcw %0"::"m"(fpu_cw));
 	kmain();
 }
--- stage3/main.c
+++ stage3/main.c
@@ -125,18 +125,6 @@ void kmain()
 	print_num_pad(features, 2, 32, '0');
 	print(S("\n"));
 
-	// enable SSE. long mode demands it is present
-	u64 cr0;
-	asm volatile("mov %%cr0, %0 \n":"=r"(cr0));
-	asm volatile("mov %0, %%cr0"::"r"((cr0 & ~(1 << 2)) | (1 << 1)));
-
-	u64 cr4;
-	asm volatile("mov %%cr4, %0":"=r"(cr4));
-	asm volatile("mov %0, %%cr4"::"r"(cr4 | (1 << 9) | (1 << 10)));
-
-	u16 fpu_cw = 0x37a;
-	asm volatile("fldcw %0"::"m"(fpu_cw));
-
 	interrupts_init();
 	pic_init();
 	thread_init();
